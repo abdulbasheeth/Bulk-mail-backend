05ea185ee05e113b400d989095b6edcc5a075db9
abdulbasheeth <basheethabdul1024@gmail.com>
2026-01-14 14:31:37 +0400

    Initial backend commit

---
+++ demo.js
@@ -0,0 +1,23 @@
+const  fileinput = document.getElementById("fileinput")
+
+fileinput.addEventListener("change",function(event){
+    const file = event.target.files[0]
+    console.log(file)
+
+    const reader= new FileReader()
+ 
+    reader.onload = function(e){
+             const data = e.target.result
+             const workbook =XLSX.read(data,{type:'binary'})
+             const sheetName = workbook.SheetNames[0]
+             const worksheet = workbook.Sheets[sheetName]
+             const emaillist = XLSX.utils.sheet_to_json(worksheet,{header:'A'})
+             console.log(emaillist)
+    }
+    
+
+
+
+
+    reader.readAsBinaryString(file);
+})
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const mongoose = require("mongoose")
+const app = express()
+
+
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://abdul:123@cluster0.vcb1wby.mongodb.net/passkey?appName=Cluster0").then(function () {
+  console.log("Connected to DB")
+}).catch(function () { console.log("Failed to Connect") })
+
+
+const credential = mongoose.model("credential", {}, "bulkmail")
+
+
+
+
+app.post("/sendMail", function (req, res) {
+
+  var msg = req.body.msg
+  var emailList = req.body.emailList
+
+  credential.find().then(function (data) {
+    const transporter = nodemailer.createTransport({
+
+      service: "gmail",
+
+      auth: {
+        user: data[0].toJSON().user,
+        pass: data[0].toJSON().pass,
+      },
+    });
+
+    new Promise(async function (resolve, reject) {
+      try {
+
+        for (var i=0;i<emailList.length;i++) 
+          {
+          await transporter.sendMail({
+            from: "basheethabdul1024@gmail.com",
+            to: emailList[i],
+            subject: "bulk mail App",
+            text: msg
+          }
+          )
+          console.log("Email sent to:"+emailList[i])
+        }
+        resolve("Success")
+      }
+      catch (error) {
+        reject("Failed")
+      }
+
+
+    }).then(function () {
+      res.send("Email Send Successfully")
+    }).catch(function () {
+      res.send(false)
+    })
+
+
+  }).catch(function (error) {
+    console.log(error)
+  })
+
+
+})
+
+
+app.listen(5000, function () {
+  console.log("server  Started")
+})
